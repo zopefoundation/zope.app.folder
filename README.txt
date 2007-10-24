6ce630609a6410ad6e5676444a579f43945442fd
Stephan Richter <stephan.richter@gmail.com>
2007-10-24 14:32:17 +0000

    Get ready for release.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This packages provides the standard Folder content type for Zope 3. It also
+implements the root folder of a Zope application. Folders can also be
+converted to sites, which allow one to register components locally.
--- setup.py
+++ setup.py
@@ -15,37 +15,59 @@
 
 $Id$
 """
-
 import os
+from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages, Extension
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.folder',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.folder',
-      license='ZPL 2.1',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-	  packages=find_packages('src'),
-	  package_dir = {'': 'src'},
+      description='Folder and Site -- Zope 3 Content Components',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Dcoumentation\n' +
+          '----------------------\n'
+          + '\n\n' +
+          read('src', 'zope', 'app', 'folder', 'filerepresentation.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 folder site local component",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.folder',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
       extras_require=dict(test=['zope.app.testing',
                                 'zope.app.zcmlfiles']),
-      namespace_packages=['zope', 'zope.app'],
       install_requires=['setuptools',
-                        'zope.schema',
-                        'zope.interface',
-                        'zope.event',
-                        'zope.security',
-                        'zope.dublincore',
-                        'zope.traversing',
-                        'zope.app.container',
-                        'zope.app.component',
                         'zope.datetime',
+                        'zope.dublincore',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.traversing',
                         'zope.app.authentication',
+                        'zope.app.component',
+                        'zope.app.container',
                         'zope.app.security',
                         'ZODB3',
                         ],
       include_package_data = True,
-
       zip_safe = False,
       )
