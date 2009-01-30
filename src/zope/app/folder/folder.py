0275a88078832d08107c0822b48c919577ac3a41
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2009-01-30 12:26:13 +0000

    add bbb imports for things moved to zope.container and zope.site

--- src/zope/app/folder/folder.py
+++ src/zope/app/folder/folder.py
@@ -2,18 +2,18 @@
 #
 # Copyright (c) 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-#
+# 
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-#
+# 
 ##############################################################################
 """
-BBB This module moved to zope.site
+BBB This module moved to zope.site.folder
 """
 
 # BBB
-from zope.site.interfaces import IFolder, IRootFolder
+from zope.site.folder import Folder, rootFolder, FolderSublocations
